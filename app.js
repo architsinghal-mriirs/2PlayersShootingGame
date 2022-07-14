09cf679dbe44cfa1584ceea87c93ab31be1ef382
archit singhal <zyphergiest@pop-os.localdomain>
2022-07-14 13:22:25 +0530

    Added game basic functionality for single player

--- app.js
+++ app.js
@@ -18,8 +18,17 @@ function get(element) {
 }
 
 function dmgCalculator() {
-  let baseDmg = Math.floosr(Math.random() * 9);
-  let randomizer = [0, 1];
-  let critChance = Math.random(...randomizer);
-  console.log(critChance);
+  let baseDmg = 1 + Math.floor(Math.random() * 10); // calculating base dmg. Dmg should not be 0.
+  let randomizer = [0, 0, 0, 0, 1]; // 20% chance to critical dmg
+  let randomIndex = Math.floor(Math.random() * randomizer.length);
+  if (randomizer[randomIndex] == 0) {
+    return baseDmg;
+  }
+  return baseDmg * 2;
+}
+
+function attack() {
+  let hit = dmgCalculator();
+  dmg.innerText = hit;
+  hp.value -= hit;
 }
